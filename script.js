15119d13ebd3c24cf1cd67c643c9c91564512c7f
strickergt128 <cv.1411.vargas@gmail.com>
2016-08-13 14:42:23 -0600

    Login,Registro

---
+++ script.js
@@ -0,0 +1,42 @@
+var myapp= angular.module('myapp',[]);
+/*function ModeloCtrl($scope) {
+	$scope.Nombre="Elena";
+}
+*/
+myapp.controller("ModeloCtrl", function ModeloCtrl($scope){
+
+$scope.register = function(user){
+
+	if (user === undefined || user === null) {
+		alert("Tu pinche pancres esta vacio")
+	}
+
+	$scope.nombre = user.nombre;
+	$scope.email = user.email;
+	$scope.edad = user.edad;
+	$scope.contrasena = user.contrasena;
+
+
+
+	if ($scope.nombre == undefined ) {
+		alert("El Campo Nombre esta Vacio")
+	}
+	if ($scope.email == undefined ) {
+		alert("El Campo Email esta Vacio")
+	}
+	if ($scope.edad == undefined ) {
+		alert("El Campo Edad esta vacio")
+	}
+	if ($scope.contrasena == undefined ) {
+		alert("El Campo Contraseña esta vacio")
+	}
+
+	console.log(user)
+
+
+
+
+	
+
+}
+});
