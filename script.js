424523e80aac1f0647abeabc0c357975ed292b47
Carlosvva <cv.1411.vargas@gmail.com>
2016-09-10 12:06:27 -0600

    Firebase

--- script.js
+++ script.js
@@ -12,14 +12,20 @@ $scope.register = function(user){
 	}
 
 	$scope.nombre = user.nombre;
+	$scope.apellido= user.apellido;
 	$scope.email = user.email;
 	$scope.edad = user.edad;
 	$scope.contrasena = user.contrasena;
+	$scope.contrasena2 = user.contrasena2;
+
 
 
 
 	if ($scope.nombre == undefined ) {
 		alert("El Campo Nombre esta Vacio")
+	}
+	if ($scope.apellido == undefined ) {
+		alert("El Campo apellido esta Vacio")
 	}
 	if ($scope.email == undefined ) {
 		alert("El Campo Email esta Vacio")
@@ -30,11 +36,20 @@ $scope.register = function(user){
 	if ($scope.contrasena == undefined ) {
 		alert("El Campo Contraseña esta vacio")
 	}
-
-	console.log(user)
-
-
-
+	if ($scope.contrasena2 == undefined ) {
+		alert("El Campo de Confirmar Contraseña esta vacio")
+	}
+	if (user.contrasena == user.contrasena2) {
+		firebase.database().ref('user/').set({
+			nombre: user.nombre,
+			apellido: user.apellido,
+			email: user.email,
+			edad: user.edad,
+			contrasena: user.contrasena
+		});
+	}else{
+		alert("Las Contraseñas no coinciden")
+	}
 }
 });
 
